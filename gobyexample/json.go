bdc8a6ee2500278c4edbb8f942a54d421ffb3916
Minwoo Kim <minwoo.kim@devsisters.com>
2015-02-17 20:45:15 +0900

    add time example

--- gobyexample/json.go
+++ gobyexample/json.go
@@ -22,4 +22,54 @@ func main() {
 	fmt.Println(string(intB))
 
 	fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
+
+	strB, _ := json.Marshal("gopher")
+	fmt.Println(string(strB))
+
+	slcD := []string{"apple", "peach", "pear"}
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
+
+	mapD := map[string]int{"apple": 5, "lecture": 7}
+	mapB, _ := json.Marshal(mapD)
+
+	fmt.Println(string(mapB))
+
+	res1D := &Response1{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"},
+	}
+	res1B, _ := json.Marshal(res1D)
+	fmt.Println(string(res1B))
+
+	res2D := &Response2{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"},
+	}
+	res2B, _ := json.Marshal(res2D)
+	fmt.Println(string(res2B))
+
+	byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+
+	var dat map[string]interface{}
+
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+	fmt.Println(dat)
+
+	strs := dat["strs"].([]interface{})
+	str1 := strs[0].(string)
+	fmt.Println(str1)
+
+	str := `{"page":1, "fruits": ["apple", "peach"]}`
+	res := &Response2{}
+	json.Unmarshal([]byte(str), &res)
+	fmt.Println(res)
+	fmt.Println(res.Fruits[0])
+
+	enc := json.NewEncoder(os.Stdout)
+	d := map[string]int{"apple": 5, "lecture": 7}
+	enc.Encode(d)
 }
